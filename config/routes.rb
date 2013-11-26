46620b96735bba24da05728504f0d70e29afa38b
Kenny <chenkenn@gmail.com>
2013-11-25 21:17:49 -0500

    fixed logout error, cleaned nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ DoubleCheck::Application.routes.draw do
 
 
   devise_for :users
-  match "places", :to => "check_lists#index"
-  match "places/:subtype", :to => "check_lists#new", :as => "places"
-  match "climate/:subtype", :to => "check_lists#new", :as => "climate"
-  match "local/:subtype", :to => "check_lists#new", :as => "local"
   resources :items, :check_lists, :reviews
   root :to => 'static_pages#index'
 end
